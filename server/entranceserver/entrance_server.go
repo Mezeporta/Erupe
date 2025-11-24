a992c5a603f2077aac1cb36d54d2b979d7d486e3
Houmgaor <contact@houmgaor.com>
2025-11-24 17:59:41 +0100

    refactor(format): automated formatting with go fmt.

--- server/entranceserver/entrance_server.go
+++ server/entranceserver/entrance_server.go
@@ -68,7 +68,7 @@ func (s *Server) Shutdown() {
 	s.listener.Close()
 }
 
-//acceptClients handles accepting new clients in a loop.
+// acceptClients handles accepting new clients in a loop.
 func (s *Server) acceptClients() {
 	for {
 		conn, err := s.listener.Accept()
